5fbc6247e0bc51377927297ac1c52fbbd40fd066
icicle <unknown@icicleling.com>
2021-07-25 23:26:25 +0800

    feat: use strings builder

--- output.go
+++ output.go
@@ -30,11 +30,11 @@ func Output(dataUrlFlag *bool, printFlag *bool, binFlag *bool) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		str := ""
+		var stringBuilder strings.Builder
 		for i := 0; i < len(byteArr); i++ {
-			str += fmt.Sprintf("%b ", byteArr[i])
+			stringBuilder.WriteString(fmt.Sprintf("%b ", byteArr[i]))
 		}
-		resultStr = strings.TrimSpace(str)
+		resultStr = strings.TrimSpace(stringBuilder.String())
 	} else {
 		base64Str, err := util.GetBase64ByFilePath(originPathStr)
 		if err != nil {
